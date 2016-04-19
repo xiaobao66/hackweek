6b3fa0eec63c6cd3543c5efb62beb06d34f0f191
weidabao <wei.yucheng@foxmail.com>
2016-04-19 20:34:22 +0800

    添加fastclick，修复点透bug

--- mobile/scripts/dist/index.js
+++ mobile/scripts/dist/index.js
@@ -44,11 +44,17 @@
 /* 0 */
 /***/ function(module, exports) {
 
+	var attachFastClick = Origami.fastclick;
+	attachFastClick(document.body);
 	$('#search-input').on('focus', function () {
 	    $('.search').addClass('show-cancel-btn');
 	});
 
-	$('.cancel-btn').on('tap', function (e) {
+	$('#search-input').on('blur', function () {
+	    $('.search').removeClass('show-cancel-btn');
+	});
+
+	$('.cancel-btn').on('click', function (e) {
 	    e.preventDefault();
 	    $('#search-input').val('').blur();
 	    $('.search').removeClass('show-cancel-btn');
